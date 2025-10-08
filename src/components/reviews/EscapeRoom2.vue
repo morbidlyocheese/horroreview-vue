3bfca87cabcfc354058446e8ca7d6f44bc21839d
Dandelion Yuen <morbidlyocheese@gmail.com>
2025-10-08 00:29:02 -0700

    updated escape room; ntf 🐛

--- src/components/reviews/EscapeRoom2.vue
+++ src/components/reviews/EscapeRoom2.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>{{ title }}</h1>
-        <!-- <img alt={{ cover }} class="cover-img" src={{ src }} width="125" height="125" /> -->
+        <img alt={{ cover }} class="cover-img" :src="img" width="125" height="125" />
         <div>
             <p class="text">{{ text }}</p>
             
@@ -10,11 +10,12 @@
 </template>
 
 <script>
+    import img from '../../assets/covers/escape-room-2-poster.jpg'
     export default {
         data() {
             return {
                 cover: 'escape room 2 movie poster',
-                src: '',
+                // src: '../../assets/covers/escape-room-2-poster.jpg',
                 title: 'Escape Room: Tournament Of Champions',
                 text: `I feel like the like back story of this film was kind of good? But I also just feel like they dragged it on.`,
                 stars: '',
