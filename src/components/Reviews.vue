c7034c27617887684859f1f5e6f4c8016ef501ab
Dandelion Yuen <morbidlyocheese@gmail.com>
2025-10-12 14:52:12 -0700

    reviews link evenly spaced fixed

--- src/components/Reviews.vue
+++ src/components/Reviews.vue
@@ -16,6 +16,14 @@
                 </li>
             </ul>
         </div>
+        <div class="tv-links">
+            <h3>TV Reviews:</h3>
+            <ul>
+                <li v-for="(tv, index) in tvs" :key="`tv-` + index">
+                    <RouterLink class="link" :to="tv.route">{{ tv.title }}</RouterLink>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
@@ -35,6 +43,12 @@ export default {
                     title: 'Escape Room: Tournament Of Champions',
                     route: '/reviews/escape-room-2'
                 }
+            ],
+            tvs: [
+                {
+                    title: 'Day Of The Dead',
+                    route: '/reviews/day-of-the-dead'
+                }
             ]
         }
     }
@@ -44,28 +58,50 @@ export default {
 
 <style>
     ul {
-        list-style-type: none;
+        list-style: none;
+        padding: 0;
+        margin: 0;
     }
 
     h3 {
         text-decoration: underline;
+        margin: 0 0 0.5rem 0;
     }
 
     .link {
-        display: flex;
+        display: inline-flex;
+        align-items: center;
     }
 
     .links {
-        display: flex;
-        flex-direction: row;
-        gap: 10rem;
+        display: grid;
+        grid-template-columns: repeat(3, minmax(0, 1fr));
+        gap: 2rem;
+        align-items: start;
         place-items: center;
+        width: 100%;
+        box-sizing: border-box;
     }
 
-    .book-links, .film-links {
+    .book-links, .film-links, .tv-links {
         display: flex;
         flex-direction: column;
         gap: 0.5rem;
-        place-items: center;
+        align-items: flex-start;
+    }
+
+    li {
+        margin: 0.25rem 0;
+    }
+
+    @media (max-width: 720px) {
+        .links {
+            grid-template-columns: 1fr;
+            gap: 1rem;
+        }
+
+        .book-links, .film-links, .tv-links {
+            align-items: center;
+        }
     }
 </style>
